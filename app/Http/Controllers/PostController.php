66717deb7c9ab638ca71b9f756931cf3e0ddc7a2
Hiromu906 <hiromu.s.0906@gmail.com>
2023-07-12 23:35:37 +0900

    ブログ投稿画面の保存処理,入力チェック追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,15 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
-
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]); 
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(10)]); 
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
        //getByLimit()で()内にある数だけデータを読み込むようにする
     }
@@ -17,12 +16,15 @@ class PostController extends Controller
     {
         return view('posts.show')->with(['post' => $post]);
     }
-    public function create(Post $post)
-    {
-        return view('posts.create')-> with(['post'=> $post]);
+    public function create(){
+        return view('posts.create');
     }//create関数からcreate.blade.phpを呼び出し
-    public function store(Post $post){
-        return view('posts.store')-> with(['post'=> $post]);
+    public function store(PostRequest $request, Post $post){
+
+        $input = $request['post'];
+        
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
 ?>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,17 +6,19 @@
     </head>
     <body>
         <h1>Blog Name</h1>
-        <form action = "posts" method = "POST">
+        <form action = "/posts" method = "POST">
             @csrf
             <div class = "title">
                 <h2>Title</h2>
-                <input type = "text" name = "post[title]" placeholder = "タイトル"/>
+                <input type = "text" name = "post[title]" placeholder = "タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
-            <div>
+            <div class ="body">
                 <h2>Body</h2>
-                <textarea name = "post[Body]" placeholder = "本文"></textarea>
+                <textarea name = "post[body]" placeholder = "本文" >{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
-            <input type ="submit" value = "store"/>
+            <input type ="submit" value = "保存"/>
         </form> 
         <div class = "footer">
             <a href = "/">戻る</a>
